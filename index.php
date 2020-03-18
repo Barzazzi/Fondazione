fe94e43eba3a8ec2dd5a29f954efc8968cf29e9a
Daniele <Barzazzi>
2020-03-18 17:25:42 +0100

    color

--- index.php
+++ index.php
@@ -11,88 +11,90 @@
     <title>Index</title>
   </head>
   <body class="sfondo">
-    <div class="row ">
-      <div class="col-4 text-center">
-        <img src="placeholder.png" class="logo ml-3">
+    <header class="sfondo-logo">
+      <div class="row mx-0 px-0">
+        <div class="col-4 text-center">
+          <img src="placeholder.png" class="logo ml-3">
+        </div>
+        <div class="col-8">
+          <h2 class="titolo pt-3">Green Sciences for Sustenaible Development</h2>
+        </div>
       </div>
-      <div class="col-8">
-        <h2 class="titolo pt-3">Green Sciences for Sustenaible Development</h2>
-      </div>
-    </div>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item active">
-            <a class="nav-link mx-lg-2" href="index.php" >Home <span class="sr-only">(current)</span></a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link mx-lg-2" href="#" >Project</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link mx-lg-2" href="#">Collaboration</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link mx-lg-2" href="#">About Us</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link mx-lg-2" href="#">Contacts</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link mx-lg-2" href="#">Collaboration</a>
-          </li>
-        </ul>
-        <form class="form-inline my-2 my-lg-0">
-          <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-        </form>
-      </div>
-    </nav>
-    <div class="row">
-      <div class="col-sm-3"></div>
-      <div class="col-sm-6">
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link mx-lg-2" href="index.php" >Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link mx-lg-2" href="#" >Project</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link mx-lg-2" href="#">Collaboration</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link mx-lg-2" href="#">About Us</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link mx-lg-2" href="#">Contacts</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link mx-lg-2" href="#">Collaboration</a>
+            </li>
+          </ul>
+          <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+          </form>
+        </div>
+      </nav>
+    </header>
+    <div class="row mx-0 px-0">
+      <div class="col-sm-2"></div>
+      <div class="col-sm-8 pt-2">
         <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
           <ol class="carousel-indicators">
             <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
             <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
             <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
           </ol>
-            <div class="carousel-inner">
-              <div class="carousel-item active">
-                <img class="d-block w-100" src="chemistry.jpg" alt="First slide">
-                <div class="carousel-caption d-none d-md-block">
-                  <h5>Prima</h5>
-                  <p>Prima</p>
-                </div>
-              </div>
-              <div class="carousel-item">
-                <img class="d-block w-100" src="chemistry.jpg" alt="Second slide">
-                <div class="carousel-caption d-none d-md-block">
-                  <h5>Seconda</h5>
-                  <p>Seconda</p>
-                </div>
-              </div>
-              <div class="carousel-item">
-                <img class="d-block w-100" src="chemistry.jpg" alt="Third slide">
-                <div class="carousel-caption d-none d-md-block">
-                  <h5>Terza</h5>
-                  <p>Terza</p>
-                </div>
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img class="d-block w-100" src="chemistry.jpg" alt="First slide">
+              <div class="carousel-caption d-none d-md-block">
+                <h5>Title 1</h5>
+                <p>Subtitle 1</p>
               </div>
             </div>
-            <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-              <span class="sr-only">Previous</span>
-            </a>
-            <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-              <span class="carousel-control-next-icon" aria-hidden="true"></span>
-              <span class="sr-only">Next</span>
-            </a>
+            <div class="carousel-item">
+              <img class="d-block w-100" src="chemistry.jpg" alt="Second slide">
+              <div class="carousel-caption d-none d-md-block">
+                <h5>Title 2</h5>
+                <p>Subtitle 2</p>
+              </div>
             </div>
+            <div class="carousel-item">
+              <img class="d-block w-100" src="chemistry.jpg" alt="Third slide">
+              <div class="carousel-caption d-none d-md-block">
+                <h5>Title 3</h5>
+                <p>Subtitle 3</p>
+              </div>
+            </div>
+          </div>
+          <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+          </a>
         </div>
-      <div class="col-sm-3"></div>
+      </div>
+      <div class="col-sm-2"></div>
     </div>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
