cf9bd9e6ba626d60a36d0888d20a11da506c6735
siviii95 <34916671+siviii95@users.noreply.github.com>
2020-03-19 11:10:29 +0100

    modificato link e aggiunta immagine articolo

--- article.php
+++ article.php
@@ -54,18 +54,18 @@
       <div class="col-lg-8 col-sm-10 pt-2">
         <div class="blog-post">
             <h2 class="blog-post-title">Call for IUPAC Postgraduate Summer School on Green Chemistry</h2>
-            <p class="blog-post-meta">Posted on 7-14 July 2018 by <a href="#">Tundo</a>, Venice Italy</p>
-            <img src="chemistry.jpg" class="img-fluid img-thumbnail" alt="Responsive image">
+            <p class="blog-post-meta">Posted on 7-14 July 2018 by <a href="#" class="link">Tundo</a>, Venice Italy</p>
+            <img src="articolo1.png" class="img-fluid img-thumbnail" alt="Responsive image">
 
            <p>
-            This Summer School will ideally follow the 10 previous editions held in Venice from 1998 to 2008, but this will be the first one under the <a href="https://iupac.org/">International Union of Pure and Applied Chemistry IUPAC</a> sponsorship.
+            This Summer School will ideally follow the 10 previous editions held in Venice from 1998 to 2008, but this will be the first one under the <a href="https://iupac.org/" class="link">International Union of Pure and Applied Chemistry IUPAC</a> sponsorship.
            </p>
            <hr>
            <p>
-            Green Chemistry is one of the key sciences of a sustainable development on earth and required for reaching the <a href="http://www.un.org/sustainabledevelopment/sustainable-development-goals/">17 UN-Goals</a>: IUPAC and ICGCSD are fully aware of their responsibility in working in the direction of Sustainable Development through Green Chemistry.
+            Green Chemistry is one of the key sciences of a sustainable development on earth and required for reaching the <a href="http://www.un.org/sustainabledevelopment/sustainable-development-goals/" class="link">17 UN-Goals</a>: IUPAC and ICGCSD are fully aware of their responsibility in working in the direction of Sustainable Development through Green Chemistry.
            </p>
            <p>
-           The School will be partly hosted in the prestigious <strong>Palazzo Ducale</strong>, the Doge’s Palace, in St. Mark’s Square thanks to the collaboration with the the Superintendence of <a href="http://www.soprintendenza.venezia.beniculturali.it/">Archeology, Fine Arts and Landscape for the City of Venice and Lagoon</a>.
+           The School will be partly hosted in the prestigious <strong>Palazzo Ducale</strong>, the Doge’s Palace, in St. Mark’s Square thanks to the collaboration with the the Superintendence of <a href="http://www.soprintendenza.venezia.beniculturali.it/" class="link">Archeology, Fine Arts and Landscape for the City of Venice and Lagoon</a>.
            </p>
            <p>
            About 80 postgraduate students and 20 teachers coming from all over the world are expected to attend. Scholarships will be offered to young talented chemists coming from developing countries.           
