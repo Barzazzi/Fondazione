583a298f4e9facce4f86659681e48a6d053e1724
Daniele <Barzazzi>
2020-03-19 14:23:57 +0100

    aaa

--- index.php
+++ index.php
@@ -41,9 +41,6 @@
             <li class="nav-item">
               <a class="nav-link mx-lg-2" href="#">Contacts</a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link mx-lg-2" href="#">Collaboration</a>
-            </li>
           </ul>
           <form class="form-inline my-2 my-lg-0">
             <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
@@ -97,6 +94,26 @@
           <div clas="col-xs-12 col-sm-5">
             <img src="placeholder.png" class="mr-3" height="215 px" width="360 px">
           </div>
+          <div class="col-xs-12 col-sm-7 pt-1">
+            <h3>Call for IUPAC Postgraduate Summer School on Green Chemistry</h3>
+            <p class="lead"><small>This Summer School will ideally follow the 10 previous editions held in Venice from 1998 to 2008, but this will be the first one under the International Union of Pure and Applied Chemistry IUPAC sponsorship.</small></p>
+            <a class="float-right link" href="#">Read more >></a>
+          </div>
+        </div>
+        <div class="row mt-3 mx-0 articolo">
+          <div clas="col-xs-12 col-sm-5">
+            <img src="placeholder.png" class="mr-3" height="215 px" width="360 px">
+          </div>
+          <div class="col-xs-12 col-sm-7">
+            <h1 class="display-4">Article title</h1>
+            <p class="lead">The subtitle of the article.</p>
+            <a class="float-right link" href="#">Read more >></a>
+          </div>
+        </div>
+        <div class="row mt-3 mx-0 articolo">
+          <div clas="col-xs-12 col-sm-5">
+            <img src="placeholder.png" class="mr-3" height="215 px" width="360 px">
+          </div>
           <div class="col-xs-12 col-sm-7">
             <h1 class="display-4">Article title</h1>
             <p class="lead">The subtitle of the article.</p>
@@ -106,6 +123,42 @@
       </div>
       <div class="col-sm-2"></div>
     </div>
+    <footer class="footer mt-3">
+      <div class="sponsor pt-3">
+        <div class="container">
+          <div class="row mx-0 px-0">
+            <div class="col-4 col-md-4">
+              <div class="text-center">
+                <h5>Sponsor by</h5>
+                <img width="200" hight="200" src="sponsor.png">
+              </div>
+            </div>
+            <div class="col-4 col-md-4">
+              <div class="text-center">
+                <h5>Sponsor by</h5>
+                <img width="200" hight="200" src="sponsor.png">
+              </div>
+            </div>
+            <div class="col-4 col-md-4">
+              <div class="text-center">
+                <h5>Sponsor by</h5>
+                <img class="mb-10" width="200" hight="200" src="sponsor.png">
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="info mt-3 p-0 pt-3">
+        <div class="row mx-0 px-0">
+          <div class="col-7">
+            <p>Foundation Green Sciences for Sustenaible Development</p>
+          </div>
+          <div class="col-5">
+            <a class="legal" href="https://it.wikipedia.org/wiki/Legale">Legal notes</a>
+          </div>
+        </div>
+      </div>
+    </footer>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
@@ -113,42 +166,4 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     <script scr="script.js"></script>
   </body>
-  <footer class="footer">
-    <div class="sposnor">
-      <div class="container">
-        <div class="row">
-          <div class="col-4 col-md-4">
-            <div class="text-center">
-              <h5>Sponsor by</h5>
-              <img width="200" hight="200" src="sponsor.png">
-            </div>
-          </div>
-          <div class="col-4 col-md-4">
-            <div class="text-center">
-              <h5>Sponsor by</h5>
-              <img width="200" hight="200" src="sponsor.png">
-            </div>
-          </div>
-          <div class="col-4 col-md-4">
-            <div class="text-center">
-              <h5>Sponsor by</h5>
-              <img class="mb-10" width="200" hight="200" src="sponsor.png">
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="info">
-      <div class="container">
-        <div class="row">
-          <div class="col-8">
-            <p>hdsjgsdkgjhdasdadgkdjgadgkdjgdjgdkhaghasdd</p>
-          </div>
-          <div class="col-4">
-          <a class="legal" href="https://it.wikipedia.org/wiki/Legale">Note Legali</a>
-          </div>
-        </div>
-      </div>
-    </div>
-  </footer>
 </html>
