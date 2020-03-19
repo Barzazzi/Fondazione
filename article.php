1d9e7d1658811495ce035583748984cfe0f1f336
siviii95 <34916671+siviii95@users.noreply.github.com>
2020-03-19 11:06:37 +0100

    aggiunto articolo di prova

---
+++ article.php
@@ -0,0 +1,96 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <title>Article</title>
+  </head>
+  <body class="sfondo">
+    <header class="sfondo-logo">
+      <div class="row mx-0 px-0">
+        <div class="col-4 text-center">
+          <img src="placeholder.png" class="logo ml-3">
+        </div>
+        <div class="col-8">
+          <h2 class="titolo pt-3">Green Sciences for Sustenaible Development</h2>
+        </div>
+      </div>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active border-right">
+              <a class="nav-link mx-lg-2" href="index.php" >Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item active border-right">
+              <a class="nav-link mx-lg-2" href="#" >Project</a>
+            </li>
+            <li class="nav-item active border-right">
+              <a class="nav-link mx-lg-2" href="#">Collaboration</a>
+            </li>
+            <li class="nav-item active border-right">
+              <a class="nav-link mx-lg-2" href="#">About Us</a>
+            </li>
+            <li class="nav-item active border-right">
+              <a class="nav-link mx-lg-2" href="#">Contacts</a>
+            </li>
+          </ul>
+          <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+          </form>
+        </div>
+      </nav>
+    </header>
+    <div class="row mx-0 px-0">
+      <div class="col-lg-2 col-sm-1"></div>
+      <div class="col-lg-8 col-sm-10 pt-2">
+        <div class="blog-post">
+            <h2 class="blog-post-title">Call for IUPAC Postgraduate Summer School on Green Chemistry</h2>
+            <p class="blog-post-meta">Posted on 7-14 July 2018 by <a href="#">Tundo</a>, Venice Italy</p>
+            <img src="chemistry.jpg" class="img-fluid img-thumbnail" alt="Responsive image">
+
+           <p>
+            This Summer School will ideally follow the 10 previous editions held in Venice from 1998 to 2008, but this will be the first one under the <a href="https://iupac.org/">International Union of Pure and Applied Chemistry IUPAC</a> sponsorship.
+           </p>
+           <hr>
+           <p>
+            Green Chemistry is one of the key sciences of a sustainable development on earth and required for reaching the <a href="http://www.un.org/sustainabledevelopment/sustainable-development-goals/">17 UN-Goals</a>: IUPAC and ICGCSD are fully aware of their responsibility in working in the direction of Sustainable Development through Green Chemistry.
+           </p>
+           <p>
+           The School will be partly hosted in the prestigious <strong>Palazzo Ducale</strong>, the Doge’s Palace, in St. Mark’s Square thanks to the collaboration with the the Superintendence of <a href="http://www.soprintendenza.venezia.beniculturali.it/">Archeology, Fine Arts and Landscape for the City of Venice and Lagoon</a>.
+           </p>
+           <p>
+           About 80 postgraduate students and 20 teachers coming from all over the world are expected to attend. Scholarships will be offered to young talented chemists coming from developing countries.           
+           </p>
+           <p>
+           The choice of Venice, this extraordinary but delicate architectural masterpiece between land and sea, as Host city for the Summer School has offered the opportunity to highlight the emerging connection of Green Chemistry with cultural heritage restoration and conservation field.           
+           </p>
+           <h3>Main topics</h3>
+            <ul>
+                <li>Chemistry beyond chlorine</li>
+                <li>New reaction pathways</li>
+                <li>Exploitation of renewable resources</li>
+                <li>Green Chemistry for cultural heritage restoration</li>
+            </ul>
+        </div>
+        
+        
+      </div>
+      <div class="col-lg-2 col-sm-1"></div>
+    </div>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script scr="script.js"></script>
+  </body>
+</html>
