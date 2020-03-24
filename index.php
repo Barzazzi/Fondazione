c08dce891c9e5ba5f615a7d53e517e0adde85154
Daniele <Barzazzi>
2020-03-24 15:48:47 +0100

    header change

--- index.php
+++ index.php
@@ -15,41 +15,45 @@
   <body class="sfondo">
     <header class="sfondo-logo">
       <div class="row mx-0 px-0">
-        <div class="col-3 text-center">
-          <img src="manovuota.jpg" class="logo ml-3">
+        <div class="col-sm-2"></div>
+        <div class="col-sm-8">
+          <img src="LOGO.gif">
         </div>
-        <div class="col-9">
-          <h2 class="display-4 titolo pt-3">Green Sciences for Sustenaible Development</h2>
+        <div class="col-sm-2"></div>
+        <div class="col-sm-2"></div>
+        <div class="col-sm-8 px-0">
+          <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active border-right">
+                  <a class="nav-link mr-lg-2" href="index.php" >Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item active border-right">
+                  <a class="nav-link mx-lg-2" href="#" >Project</a>
+                </li>
+                <li class="nav-item active border-right">
+                  <a class="nav-link mx-lg-2" href="#">Collaboration</a>
+                </li>
+                <li class="nav-item active border-right">
+                  <a class="nav-link mx-lg-2" href="#">About Us</a>
+                </li>
+                <li class="nav-item active border-right">
+                  <a class="nav-link mx-lg-2" href="#">Contacts</a>
+                </li>
+              </ul>
+              <form class="form-inline">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+              </form>
+            </div>
+          </nav>
         </div>
+        <div class="col-sm-2"></div>
       </div>
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-            <li class="nav-item active border-right">
-              <a class="nav-link mx-lg-2" href="index.php" >Home <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item active border-right">
-              <a class="nav-link mx-lg-2" href="#" >Project</a>
-            </li>
-            <li class="nav-item active border-right">
-              <a class="nav-link mx-lg-2" href="#">Collaboration</a>
-            </li>
-            <li class="nav-item active border-right">
-              <a class="nav-link mx-lg-2" href="#">About Us</a>
-            </li>
-            <li class="nav-item active border-right">
-              <a class="nav-link mx-lg-2" href="#">Contacts</a>
-            </li>
-          </ul>
-          <form class="form-inline my-2 my-lg-0">
-            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-          </form>
-        </div>
-      </nav>
+      
     </header>
     <div class="row mx-0 px-0">
       <div class="col-sm-2"></div>
