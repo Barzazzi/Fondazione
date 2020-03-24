fec6ea7e68eed078c90d4362ff678705e627e521
Daniele <Barzazzi>
2020-03-24 16:52:38 +0100

    aa

--- header.php
+++ header.php
@@ -8,7 +8,7 @@
 <div class="row mx-0 px-0">
     <div class="col-sm-2"></div>
     <div class="col-sm-8">
-        <img src="Logo.gif">
+        <img src="Logo.gif" class="img-responsive">
     </div>
     <div class="col-sm-2"></div>
     <div class="col-sm-2"></div>
